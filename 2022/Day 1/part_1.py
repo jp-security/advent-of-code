4645fa211ff12845dd05fe4dc43985bd85172d52
J2 <josh_pachmayer@yahoo.com>
2022-12-01 00:33:02 -0500

    Day 1 Complete

---
+++ 2022/Day 1/part_1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+file = open("./input.text", 'r')
+df_setup = []
+elf_count = 0
+count = 0
+previous_sum = 0
+while True:
+    count += 1
+    line = file.readline()
+
+    if not line:
+        break        
+
+    if line == '\n':
+        df_setup.append({'Elf Name': f'elf-{elf_count}', 'Calories': previous_sum})
+        elf_count += 1
+        previous_sum = 0
+        continue
+    previous_sum += int(line)
+    
+file.close()
+
+df = pd.DataFrame(df_setup)
+df = df.sort_values(by=["Calories"], ascending=False)
+print(df)
+print(df.head(3).Calories.sum())
