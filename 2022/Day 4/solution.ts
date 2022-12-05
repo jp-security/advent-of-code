8bb43080e3eb5f48689fe9afdb6ac68099435f22
J2 <josh_pachmayer@yahoo.com>
2022-12-05 10:31:06 -0500

    Day 4 Solution

---
+++ 2022/Day 4/solution.ts
@@ -0,0 +1,41 @@
+import { parseData } from "../../utils/parsers";
+const data = parseData(__dirname, 'input.txt');
+
+let partOneOverLapCounter: number = 0;
+let partTwoOverLapCounter: number = 0;
+
+function testRange(n1: number, n2: number, t1:number){
+    if (n1 <= t1 && t1 <= n2){
+        return true
+    };
+};
+
+data.forEach(element => {
+    const [firstPlot, secondPlot] = element.split(',', 2);
+    const [firstZone, secondZone] = firstPlot.split('-', 2);
+    const [thirdZone, fourthZone] = secondPlot.split('-', 2);
+
+    if (testRange(+firstZone, +secondZone, +thirdZone) && testRange(+firstZone, +secondZone, +fourthZone)){
+        partOneOverLapCounter += 1;
+    } else if (testRange(+firstZone, +secondZone, +fourthZone) && testRange(+firstZone, +secondZone, +thirdZone)) {
+        partOneOverLapCounter += 1;
+    }else if (testRange(+thirdZone, +fourthZone, +firstZone) && testRange(+thirdZone, +fourthZone, +secondZone)){
+        partOneOverLapCounter += 1;
+    } else if (testRange(+thirdZone, +fourthZone, +secondZone) && testRange(+thirdZone, +fourthZone, +firstZone)){
+        partOneOverLapCounter += 1;
+    };
+
+    if (testRange(+firstZone, +secondZone, +thirdZone) || testRange(+firstZone, +secondZone, +fourthZone)){
+        partTwoOverLapCounter += 1;
+    } else if (testRange(+firstZone, +secondZone, +fourthZone) || testRange(+firstZone, +secondZone, +thirdZone)) {
+        partTwoOverLapCounter += 1;
+    }else if (testRange(+thirdZone, +fourthZone, +firstZone) || testRange(+thirdZone, +fourthZone, +secondZone)){
+        partTwoOverLapCounter += 1;
+    } else if (testRange(+thirdZone, +fourthZone, +secondZone) || testRange(+thirdZone, +fourthZone, +firstZone)){
+        partTwoOverLapCounter += 1;
+    };
+});
+
+
+console.log(`Part One: ${partOneOverLapCounter}`);
+console.log(`Part Tne: ${partTwoOverLapCounter}`);
